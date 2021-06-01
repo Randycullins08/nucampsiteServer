7337987263f1813c67532132424c6b6c6bd90470
Randy Cullins <randycullins2018@gmail.com>
2021-06-01 15:05:32 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "499381471496578",
+    clientSecret: "5b7bc41075191726862e944a2c22c6c0",
+  },
 };
